6e113eba928eaa27800ebf96cac433e2f6b15632
mota3000 <mota3000.1@gmail.com>
2025-11-11 20:10:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿//1. NOT UNDERSTAND
+//2. IT'S IMPOSSIBLE
+//3. if(x <= 10 && x >= 20)
+//4. с помощья команды break()
+//5. IT'S IMPOSSIBLE
+//6. в массиве [] нужно указать размер, а в List
+//7. 
+//
+//
+//
